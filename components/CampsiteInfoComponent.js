ccd4cbf69fc8d057865023216717b7ec817bc556
Raphael Cruz <raphaelxlive@hotmail.com>
2021-04-22 17:28:15 -0400

    added favorite to the directory component

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Icon } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
-function RenderCampsite({ campsite }) {
+function RenderCampsite(props) {
+  const { campsite } = props;
+
   if (campsite) {
     return (
       <Card
@@ -10,14 +13,55 @@ function RenderCampsite({ campsite }) {
         image={require("./images/react-lake.jpg")}
       >
         <Text style={{ margin: 10 }}>{campsite.description}</Text>
+        <Icon
+          name={props.favorite ? "heart" : "heart-o"}
+          type="font-awesome"
+          color="#f50"
+          raised
+          reverse
+          onPress={() =>
+            props.favorite
+              ? console.log("Already set as a favorite")
+              : props.markFavorite()
+          }
+        />
       </Card>
     );
   }
   return <View />;
 }
 
-function CampsiteInfo(props) {
-  return <RenderCampsite campsite={props.campsite} />;
+class CampsiteInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+
+      favorite: false,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Campsite Information",
+  };
+
+  markFavorite() {
+    this.setState({ favorite: true });
+  }
+
+  render() {
+    const campsiteId = this.props.navigation.getParam("campsiteId");
+    const campsite = this.state.campsites.filter(
+      (campsite) => campsite.id === campsiteId
+    )[0];
+    return (
+      <RenderCampsite
+        campsite={campsite}
+        favorite={this.state.favorite}
+        markFavorite={() => this.markFavorite()}
+      />
+    );
+  }
 }
 
 export default CampsiteInfo;
